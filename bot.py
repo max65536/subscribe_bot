de730c5fe5286ebc1422843bdfc150201416237b
max65536 <maxinteger65536@gmail.com>
2023-09-14 15:33:12 +0800

    support delete and letter case

--- bot.py
+++ bot.py
@@ -17,16 +17,13 @@ bot_token = os.getenv('BOT_TOKEN', '')
 bot_client = TelegramClient('bot', user_api_id, user_api_hash).start(bot_token=bot_token)
 
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.INFO)
+                    level=logging.WARNING)
 
 # database
 client = MongoClient()
 db = client['tgbot']
 user_collection = db['users']
 channel_collection = db['channels']
-
-channels = ["Odaily", "BlockBeats", "Foresight News"]
-keywords = ["ETH", "layer2", "BTC", "分片", "eip"]
 
 def init_user_in_db(userid):
     existing_user = user_collection.find_one({"userid": userid})
@@ -78,6 +75,34 @@ def append_keywords_in_db(words, userid):
 
     return user['keywords']
 
+def del_keyword_in_db(word, userid):
+    user = user_collection.find_one_and_update(
+        {"userid":userid},
+        {'$pull':{'keywords':word}},
+        return_document=ReturnDocument.AFTER
+    )
+
+    channel_collection.update_many(
+        {"users.id":userid},
+        {'$pull':{"users.$.words": word}}
+    )
+
+    return user['keywords']
+
+def del_channel_in_db(channel_name, userid):
+    user = user_collection.find_one_and_update(
+        {"userid":userid},
+        {'$pull':{'channels':channel_name}},
+        return_document=ReturnDocument.AFTER
+    )
+
+    channel_collection.update_many(
+        {"title":channel_name},
+        {'$pull':{"users": {"id":userid}}}
+    )
+    
+    return user['channels']
+
 @user_client.on(events.NewMessage())
 async def message_handler(event):
     # logging.info(event.message)
@@ -86,12 +111,9 @@ async def message_handler(event):
     channel = channel_collection.find_one({"channelid": sender.id}) 
     for user in channel['users']:
         for word in user['words']:
-            if word in event.message.text:
-                await bot_client.send_message(user['id'], "From %s:\n%s"%(sender.title, event.message.text))    
-    
-# logging.info(str(sender)+":"+event.message.text)
-#    await bot_client.send_message(28664002, event.message.text)
- 
+            if word.lower() in event.message.text.lower():
+                await bot_client.send_message(user['id'], "From %s:\n%s"%(sender.title, event.message.text)) 
+                break       
 
 # 为机器人定义事件处理程序
 @bot_client.on(events.NewMessage(pattern='/start'))
@@ -99,11 +121,7 @@ async def bot_handler(event):
     sender = await event.get_sender()
     init_user_in_db(sender.id)
     await event.respond(
-        'Welcome!'#,
-        # buttons=[
-        #     [Button.inline('check channels', b'get_channels')],
-        #     [Button.inline('check keywords', b'get_keywords')]
-        # ]
+        'Welcome!\ntype /set_channel or /set_keyword to subscribe your interests in message streams.'
     )
 
 @bot_client.on(events.NewMessage(pattern='/show_channels'))
@@ -120,16 +138,33 @@ async def show_keywords(event):
 
 @bot_client.on(events.NewMessage(pattern='/set_channel'))
 async def set_channel(event):
-    await event.respond('input a channel name or link:', reply_to=event.id, buttons=CustomButton.force_reply(selective=True, placeholder=r'"xxx" or "https://t.me/xxx"'))    
+    await event.respond('input a channel name or link:', buttons=CustomButton.force_reply(selective=True, placeholder=r'"xxx" or "https://t.me/xxx"'))    
     
 @bot_client.on(events.NewMessage(pattern='/set_keyword'))
-async def set_channel(event):
-    await event.respond('input keywords:', reply_to=event.id, buttons=CustomButton.force_reply(selective=True, placeholder=r'BTC, ETH, layer2'))
+async def set_keyword(event):
+    await event.respond('input keywords:', buttons=CustomButton.force_reply(selective=True, placeholder=r'BTC, ETH, layer2'))
+
+@bot_client.on(events.NewMessage(pattern='/del_keyword'))
+async def del_keyword(event):
+    sender = await event.get_sender()
+    keywords = get_keywords(sender.id)
+    buttons=[
+            [Button.inline(f'{word}❌', f'dw_{word}')] for word in keywords            
+        ]
+    await event.respond("keywords:"+','.join(keywords), buttons=buttons)
+
+@bot_client.on(events.NewMessage(pattern='/del_channel'))
+async def del_channel(event):
+    sender = await event.get_sender()
+    channels = get_channels(sender.id)
+    buttons=[
+            [Button.inline(f'{channel}❌', f'dc_{channel}')] for channel in channels
+        ]
+    await event.respond("channels:"+','.join(channels), buttons=buttons)
 
 @bot_client.on(events.NewMessage(pattern='/test'))
 async def start_handler(event):
     await event.respond('请回复这条消息：', reply_to=event.id, buttons=CustomButton.force_reply(selective=True, placeholder='test placeholder'))
-
 
 @bot_client.on(events.NewMessage(func=lambda e: e.is_reply))
 async def reply_handler(event):
@@ -138,7 +173,14 @@ async def reply_handler(event):
     if replied_message.text == '请回复这条消息：':
         await event.respond(f'你说: {event.text}')
     elif replied_message.text == 'input a channel name or link:':
-        channel_link = f"https://t.me/{event.text}" if '/' not in event.text else event.text
+        
+        if event.text.startswith("https://t.me/"):
+            channel_link = event.text
+        elif event.text.startswith("t.me/"):
+            channel_link = f"https://{event.text}"
+        else:
+            channel_link = f"https://t.me/{event.text}"
+
         try:
             newchannel = await user_client.get_entity(channel_link)
             channel_result = channel_collection.find_one({"channelid": newchannel.id})
@@ -152,14 +194,14 @@ async def reply_handler(event):
                 append_user_in_channel_db(channel_result['channelid'], channel_result['title'], sender.id)
 
             logging.info("channel_result:"+str(channel_result))                
-            await event.respond(f'成功加入频道{channel_link}')
+            await event.respond(f'followed channel: {channel_link}')
         except UsernameNotOccupiedError:
-            await event.respond("该用户名不存在。")        
+            await event.respond("Error: username not exist")        
         except ChannelPrivateError:
-            await event.respond("你没有权限访问此频道，或者该频道是私有的。")            
+            await event.respond("Error: the channel is private")            
         except Exception as e:
-            logging.info(f"发生了错误：{str(e)}")                
-            await event.respond(f"发生了错误：{str(e)}")
+            logging.info(f"Error: {str(e)}")                
+            await event.respond(f"Error: {str(e)}")
     elif replied_message.text == 'input keywords:':
         replywords = event.text.replace('，',',').split(',')
         keywords = [word.strip() for word in replywords]
@@ -169,6 +211,26 @@ async def reply_handler(event):
         except Exception as e:
             logging.info(f"发生了错误：{str(e)}")                
             await event.respond(f"发生了错误：{str(e)}")            
+
+@bot_client.on(events.CallbackQuery(pattern=r'dw_(.+)'))
+async def delete_callback(event):
+    action_data = event.pattern_match.group(1).decode('utf-8')  # 获取匹配的部分并解码为字符串
+    sender = await event.get_sender()
+    keywords = del_keyword_in_db(word=action_data, userid=sender.id)
+    buttons=[
+            [Button.inline(f'{word}❌', f'dw_{word}')] for word in keywords            
+        ]    
+    await event.edit("keywords:"+','.join(keywords)+f"\n{action_data} deleted", buttons=buttons)
+
+@bot_client.on(events.CallbackQuery(pattern=r'dc_(.+)'))
+async def delete_callback(event):
+    action_data = event.pattern_match.group(1).decode('utf-8')  # 获取匹配的部分并解码为字符串
+    sender = await event.get_sender()
+    channels = del_channel_in_db(channel_name=action_data, userid=sender.id)
+    buttons=[
+            [Button.inline(f'{channel}❌', f'dw_{channel}')] for channel in channels
+        ]    
+    await event.edit("keywords:"+','.join(channels)+f"\n{action_data} deleted", buttons=buttons)
 
 @bot_client.on(events.NewMessage(pattern='/test2'))
 async def test2(event):
